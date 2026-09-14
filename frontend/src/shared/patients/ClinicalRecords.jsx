eecc0a2cd72eb167f90db403eab127566875963d
Whendelyn Bangis <bangiswhendelyn02@gmail.com>
2026-09-14 09:04:49 +0800

    Update ClinicalRecords.jsx

--- frontend/src/shared/patients/ClinicalRecords.jsx
+++ frontend/src/shared/patients/ClinicalRecords.jsx
@@ -506,7 +506,7 @@ export function ClinicalRecords({ patientId, type, role, readOnly = false }) {
             </button>
           )}
         </>
-      ) : type === "vital_signs" ? (
+      ) : type === "vital-signs" ? (
   <>
     {records.map((record) => (
       <div
